5106469955e2381772d40573d6d2d5972d3f29c8
papunjay <kumarpapunjay@gmail.com>
2020-04-06 04:02:16 -0700

    [papunjay].Add.views.Login

--- Login_Registration_Using_REST_Services/User/views.py
+++ Login_Registration_Using_REST_Services/User/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from django.shortcuts import render, redirect
+from .models import Registration
+from rest_framework.authentication import authenticate
+from rest_framework.response import Response
+from django.contrib.auth import login, logout
+from django.db.models import Q
+from rest_framework.generics import api_settings
+from rest_framework.generics import GenericAPIView
+from .serializers import (RegistrationSerializers,LoginSerializers,)
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth import authenticate, get_user_model
+from django.views.generic import TemplateView
+User = get_user_model()
