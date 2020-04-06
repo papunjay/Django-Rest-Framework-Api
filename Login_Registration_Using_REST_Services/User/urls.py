a51c41df575680f21cbdcd19191e7b8b1cb45bd6
papunjay <kumarpapunjay@gmail.com>
2020-04-06 04:23:11 -0700

    [papunjay].Add.Migrate

--- Login_Registration_Using_REST_Services/User/urls.py
+++ Login_Registration_Using_REST_Services/User/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
 from django.urls import path,include
-from User import views 
+from  User.views import Registration,Home,Login,welcome
+from django.contrib import admin
+from User.serializers import RegistrationSerializers
 
 urlpatterns = [
+    path('home/',Home.as_view(),name='home'),
+    path('register/',Registration.as_view(),name='register'),
+    path('login/',Login.as_view(),name='login'),
     path('admin/', admin.site.urls),
-    
+    path('welcome/',welcome.as_view(),name='welcome'),
+  
 ]
