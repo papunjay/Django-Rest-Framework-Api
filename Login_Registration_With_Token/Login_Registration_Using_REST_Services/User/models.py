aba0a5817e04487668eeceb183c77e962fef7788
papunjay <kumarpapunjay@gmail.com>
2020-04-17 18:23:54 +0530

    [papunjay]. refactor of models

--- Login_Registration_With_Token/Login_Registration_Using_REST_Services/User/models.py
+++ Login_Registration_With_Token/Login_Registration_Using_REST_Services/User/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth import get_user_model
+
+
 class UsersData(models.Model):
     username = models.CharField(blank=False, max_length=100)
     email = models.EmailField(blank=True)
