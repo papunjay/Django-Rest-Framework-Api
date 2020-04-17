df674a569733e3df910108b5de526c07fb385173
papunjay <kumarpapunjay@gmail.com>
2020-04-17 20:59:30 +0530

    [papunjay].Add email settings

--- Login_Registration_With_Token/Login_Registration_Using_REST_Services/Login_Registration_Using_REST_Services/settings.py
+++ Login_Registration_With_Token/Login_Registration_Using_REST_Services/Login_Registration_Using_REST_Services/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATE_DIR= os.path.join(BASE_DIR, 'User/templates') #new
 
 
@@ -142,6 +141,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mymail130801@gmail.com'
+EMAIL_HOST_PASSWORD ='k.s.v@.18'
+EMAIL_PORT = '587'
+
+
 
 
 #redis()
