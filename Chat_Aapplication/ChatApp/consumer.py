07e0c1e7a770055b8e15deda547efb4fa9b7780b
papunjay <kumarpapunjay@gmail.com>
2020-04-21 02:41:20 +0530

    [papunjay]Add chat_message function

--- Chat_Aapplication/ChatApp/consumer.py
+++ Chat_Aapplication/ChatApp/consumer.py
@@ -36,3 +36,11 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
+       # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
